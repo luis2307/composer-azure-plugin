9245b2e3649781a7ef5c4dc3a0536ae7fe3b84b7
Marvin Caspar <marvin.caspar@gmail.com>
2022-10-06 13:47:50 +0200

    Test cleanup

--- tests/AzurePluginTest.php
+++ tests/AzurePluginTest.php
@@ -5,7 +5,6 @@ use Composer\Factory;
 use Composer\IO\IOInterface;
 use Composer\Script\ScriptEvents;
 use MarvinCaspar\Composer\AzurePlugin;
-use MarvinCaspar\Composer\AzureRepository;
 use PHPUnit\Framework\TestCase;
 
 final class AzurePluginTest extends TestCase
@@ -45,7 +44,7 @@ final class AzurePluginTest extends TestCase
                 ScriptEvents::PRE_UPDATE_CMD => [['execute', 50000]],
 
                 ScriptEvents::POST_INSTALL_CMD => [['modifyComposerLockPostInstall', 50000]],
-                ScriptEvents::POST_UPDATE_CMD => [['modifyComposerLockPostInstall', 50000]]
+                ScriptEvents::POST_UPDATE_CMD => [['modifyComposerLockPostInstall', 50000]],
             ],
             AzurePlugin::getSubscribedEvents()
         );
@@ -80,75 +79,4 @@ final class AzurePluginTest extends TestCase
         $azurePlugin->expects($this->once())
             ->method('addAzurePackagesAsLocalRepositories');
     }
-
-//
-//    public function testExecuteWithAzureRepos()
-//    {
-//        $azurePlugin = $this->getMockBuilder(AzurePlugin::class)
-//            ->onlyMethods(['parseRequiredPackages', 'fetchAzurePackages', 'addAzureRepositories'])
-//            ->getMock();
-//        $azurePlugin->activate($this->composerWithAzureRepos, $this->ioMock);
-//        $this->assertTrue($azurePlugin->hasAzureRepositories);
-//        $azurePlugin->expects($this->once())
-//            ->method('parseRequiredPackages')
-//            ->with($this->composerWithAzureRepos);
-//        $azurePlugin->expects($this->once())
-//            ->method('fetchAzurePackages')
-//            ->with([]);
-//        $azurePlugin->expects($this->once())
-//            ->method('addAzureRepositories')
-//            ->with([]);
-//
-//        $azurePlugin->execute();
-//    }
-//
-//    public function testExecuteWithoutAzureReposAndInternalMocks()
-//    {
-//        $azureRepo = new AzureRepository('dev.azure.com/vendor', 'project', 'feed', false);
-//        $azureRepo->addArtifact('vendor/azure-package', '1.0.0');
-//
-//        $azurePlugin = $this->getMockBuilder(AzurePlugin::class)
-//            ->onlyMethods(['executeShellCmd', 'solveDependencies'])
-//            ->getMock();
-//        $azurePlugin->activate($this->composerWithoutAzureRepos, $this->ioMock);
-//
-//        $azurePlugin->expects($this->never())
-//            ->method('executeShellCmd');
-//
-//        $azurePlugin->expects($this->never())
-//            ->method('solveDependencies');
-//
-//        $azurePlugin->execute();
-//    }
-//
-//    public function testModifyComposerLockWithAzureRepos()
-//    {
-//        $sedCommand = 'sed -i -e "s|' . $this->cacheDir . '|~/.composer/cache/azure|g" composer.lock';
-//        // on macos sed needs an empty string for the i parameter
-//        if (strtolower(PHP_OS) === 'darwin') {
-//            $sedCommand = 'sed -i "" -e "s|' . $this->cacheDir . '|~/.composer/cache/azure|g" composer.lock';
-//        }
-//
-//        $azurePlugin = $this->getMockBuilder(AzurePlugin::class)
-//            ->onlyMethods(['executeShellCmd'])
-//            ->getMock();
-//        $azurePlugin->activate($this->composerWithAzureRepos, $this->ioMock);
-//        $azurePlugin->expects($this->once())
-//            ->method('executeShellCmd')
-//            ->with($sedCommand);
-//
-//        $azurePlugin->modifyComposerLockPostInstall();
-//    }
-//
-//    public function testModifyComposerLockWithoutAzureRepos()
-//    {
-//        $azurePlugin = $this->getMockBuilder(AzurePlugin::class)
-//            ->onlyMethods(['executeShellCmd'])
-//            ->getMock();
-//        $azurePlugin->activate($this->composerWithoutAzureRepos, $this->ioMock);
-//        $azurePlugin->expects($this->never())
-//            ->method('executeShellCmd');
-//
-//        $azurePlugin->modifyComposerLockPostInstall();
-//    }
 }
